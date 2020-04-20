cbd8807a95a80a289de71de13cd3c4a0f9529139
samarche92 <samar.chehade.sc@gmail.com>
2020-04-20 14:18:19 -0400

    display data function coded

--- main.py
+++ main.py
@@ -7,6 +7,17 @@ Created on Mon Apr 20 11:19:56 2020
 """
 
 import numpy as np
+from utils import *
 
-data=np.loadtxt("data.txt",delimiter='\t')
-labels=np.loadtxt("labels.txt",delimiter='\t')
+#Load data (handwritten images)
+# Each example is a 20x20 pixel image
+X=np.loadtxt("data.txt",delimiter='\t') 
+y=np.loadtxt("labels.txt",delimiter='\t')
+m=len(X) #number of training examples
+
+# Randomly select 100 data points to display
+rand_indices = np.random.permutation(np.arange(m))
+sel = X[rand_indices[0:100], :]
+
+#display data
+displaydata(sel)
