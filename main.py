6ff0e5d19a7cc3ae3ac545584ec745886442857a
samarche92 <samar.chehade.sc@gmail.com>
2020-04-20 11:40:16 -0400

    adding main file and loading training data

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 20 11:19:56 2020
+
+@author: samar
+"""
+
+import numpy as np
+
+data=np.loadtxt("data.txt",delimiter='\t')
+labels=np.loadtxt("labels.txt",delimiter='\t')
