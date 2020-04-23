9b1092ad8281f69ac335b254f558c4b827097269
samarche92 <samar.chehade.sc@gmail.com>
2020-04-23 11:36:28 -0400

    initializing feed_forward network

--- NeuralNetworkClass/FeedForwardClass.py
+++ NeuralNetworkClass/FeedForwardClass.py
@@ -9,8 +9,14 @@ import numpy as np
 from .NeuralNetworkClass import NeuralNetwork
 
 class FeedForward(NeuralNetwork):
+    """ Feed-forward neural networks used for classification. Inherits from 
+    general Neural Network class"""
     
     def __init__(self,input_nodes=0,num_layers=0,output_nodes=0):
+        """ Initialize the network with the number of input nodes, the number 
+        of layers (=2+ number of hidden layers) and the number of output 
+        nodes. """
+        
         NeuralNetwork.__init__(self,input_nodes,num_layers)
         self.ouput=output_nodes
         self.hidden_nodes=[0]*(num_layers-2)
@@ -18,13 +24,23 @@ class FeedForward(NeuralNetwork):
         
     def AddHiddenNodes(self,A):
         """ Function used to set the number of nodes in each layer.
-        A is an array of integers conatining the number of nodes in each hidden
+        A is a list of integers containing the number of nodes in each hidden
         layer (should be of length num_layers-2)."""
-        if len(A)==self.layers-2:
-            for (i,a) in enumerate(A):
-                self.hidden_nodes[i]=a
-        else:
+        
+        if len(A)!=self.layers-2:
             raise ValueError("Dimension of input array should be {} but instead is {}"
                              .format(self.layers-2,len(A)))
             
+        for (i,a) in enumerate(A):
+            self.hidden_nodes[i]=a
+            
     def InitializeWeights(self,W):
+        """ Function used to initialize the weights for each layer.
+        W is a list of matrices containing the initial weights for each 
+        layer. W should be of length num_layers-2 and the size of each matrix
+        is determined by the number of nodes in each layer."""
+        
+        if len(W)!=self.layers-2:
+            raise ValueError("Dimension of input array should be {} but instead is {}"
+                             .format(self.layers-2,len(W)))    
+        self.weights=W #will check each matrix size size later
